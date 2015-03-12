af5b90df12cbb340ec349cd89e4c531bc17db949
kristoff howell <kris.anto.howell@gmail.com>
2015-03-12 17:19:10 -0500

    Update Strider_Arm_Serial_Test.ino
     added the Left arm to serial communication and smooth the movements.

--- Strider_Arm_Serial_Test/Strider_Arm_Serial_Test.ino
+++ Strider_Arm_Serial_Test/Strider_Arm_Serial_Test.ino
@@ -6,7 +6,14 @@ Servo rArm_Servo3;
 Servo rArm_Servo4;
 Servo rArm_Servo5;
 
+Servo lArm_Servo1;
+Servo lArm_Servo2;
+Servo lArm_Servo3;
+Servo lArm_Servo4;
+Servo lArm_Servo5;
+
 int ang = 0;
+int currentAng = 0;
 
 char buffer[] = {' ',' ',' ',' ',' ',' ',' '}; // Receive up to 7 bytes (Used to store the input from communication)
 char pos[] = {' ', ' ', ' '};
@@ -21,11 +28,23 @@ void setup()
 	rArm_Servo4.attach(48);
 	rArm_Servo5.attach(50);
 	
+	lArm_Servo1.attach(43); // Attaches(inputs the pin out to the Servo object) class) the servos to their respective pins
+	lArm_Servo2.attach(45);
+	lArm_Servo3.attach(47);
+	lArm_Servo4.attach(49);
+	lArm_Servo5.attach(51);
+	
 	rArm_Servo1.write(5);
 	rArm_Servo2.write(5);
 	rArm_Servo3.write(5);
 	rArm_Servo4.write(5);
 	rArm_Servo5.write(5);
+	
+	lArm_Servo1.write(5);
+	lArm_Servo2.write(5);
+	lArm_Servo3.write(5);
+	lArm_Servo4.write(5);
+	lArm_Servo5.write(5);
 }
 
 void loop()
@@ -59,38 +78,196 @@ void loop()
 	}
 	*/
 	
-	
 	switch(buffer[0])
 	{
 		case 'R':
 			Serial.print("Right Arm, ");
-			Serial.print("Servo = ");
-			Serial.print(buffer[1]);
-			Serial.print(", Angle = ");
-			Serial.println(ang);
-			
 			switch(buffer[1])
 			{
 				case '1':
-					rArm_Servo1.write(ang);
+					currentAng = rArm_Servo1.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo1.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo1.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
 					break;
 				case '2':
-					rArm_Servo2.write(ang);
+					currentAng = rArm_Servo2.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo2.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo2.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
 					break;
 				case '3':
-					rArm_Servo3.write(ang);
+					currentAng = rArm_Servo3.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo3.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo3.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
 					break;
 				case '4':
-					rArm_Servo4.write(ang);
+					currentAng = rArm_Servo4.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo4.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo4.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
 					break;
 				case '5':
-					rArm_Servo5.write(ang);
+					currentAng = rArm_Servo5.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo5.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo5.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
 					break;
 				default:
 					Serial.print("Buffer[1] -> ");
 					Serial.println(buffer[1]);
 					break;
-				
+			}
+			break;
+		case 'L':
+			Serial.print("Left Arm, ");
+			switch(buffer[1])
+			{
+				case '1':
+					currentAng = rArm_Servo1.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo1.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo1.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
+					break;
+				case '2':
+					currentAng = rArm_Servo2.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo2.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo2.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
+					break;
+				case '3':
+					currentAng = rArm_Servo3.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo3.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo3.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
+					break;
+				case '4':
+					currentAng = rArm_Servo4.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo4.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo4.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
+					break;
+				case '5':
+					currentAng = rArm_Servo5.read();
+					if(currentAng < ang)
+					{
+						for(int pos = currentAng; pos < ang; pos += 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo5.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}	
+					}else{
+						for(int pos = currentAng; pos > ang; pos -= 1) 	// goes from 0 degrees to 180 degrees 
+						{						// in steps of 1 degree 
+							rArm_Servo5.write(pos);		// tell servo to go to position in variable 'pos' 
+							delay(15);			// waits 15ms for the servo to reach the position 
+						}
+					}
+					break;
+				default:
+					Serial.print("Buffer[1] -> ");
+					Serial.println(buffer[1]);
+					break;
 			}
 			break;
 		default:
@@ -99,6 +276,10 @@ void loop()
 	
 	Serial.print("Servo = ");
 	Serial.print(buffer[1]);
+	Serial.print(", CurrentAngle = ");
+	Serial.println(currentAng);
+	Serial.print(", Angle = ");
+	Serial.println(ang);
 	
 	for(int i = 0; i < 3; i++)
 	{
