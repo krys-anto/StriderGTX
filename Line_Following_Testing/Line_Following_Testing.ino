34008010d566bdb5d581368df67cac1ccd90ca3b
kristoff howell <kris.anto.howell@gmail.com>
2015-04-03 17:03:04 -0500

    Update Line_Following_Testing.ino
    added comments  and minor corrections to sections of the code

--- Line_Following_Testing/Line_Following_Testing.ino
+++ Line_Following_Testing/Line_Following_Testing.ino
@@ -1,6 +1,15 @@
-
-// Declaration of Motor Controller PINs
-#define MFR_A_PIN		37  // FRONT RIGHT MOTOR
+/********************************************************************************
+*	This code is used to test the paralax Infrared Line Follower		*
+*	along with 2 motor contorllers <insert name of motor contorller>	*
+*	and 4 motors of the strider GTX.					*
+*										*
+*	The IRF identifies and returns 0's/1's; 0's				*
+*	as white lines and 1's as black surfaces.				*
+**********************************************************************************/
+//*******************************************//
+//    Declaration of Motor Controller PINs   //
+//*******************************************//
+#define MFR_A_PIN		37  // FRONT RIGHT MOTOR 
 #define MFR_B_PIN		39 
 #define MFR_PWM_PIN		5 	// PULSE WIDTH MODULATION PIN
 
@@ -16,7 +25,9 @@
 #define MBL_B_PIN		36
 #define MBL_PWM_PIN		2 	// PULSE WIDTH MODULATION PIN 
 
-// Declaration of Parallax IR Line follower PINs
+//**************************************//
+//      Declaration of the IRF PINs     //
+//**************************************//
 #define IRL_PIN_s0	22
 #define IRL_PIN_s1	23
 #define IRL_PIN_s2	24
@@ -27,42 +38,46 @@
 #define IRL_PIN_s7	29
 
 
-
+//***********************************************//
+//    Stetting up peripherals to run code once   //
+//***********************************************//
 void setup()
 {
-	Serial.begin(9600);
-	// Setting up Motor PINs
-	pinMode(MFR_A_PIN,OUTPUT);
-	pinMode(MFR_B_PIN,OUTPUT);
-	pinMode(MFR_PWM_PIN,OUTPUT);// Controls speed- Pulse width modulation
+	Serial.begin(9600);  			// set up serial port class to establish communicaltion OUTput and INput
+	//*****************************//
+	//    Setting up Motor PINs    //
+	//*****************************//
+	pinMode(MFR_A_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MFR_B_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MFR_PWM_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
 	
 	// Motor Front Left Side
-	pinMode(MFL_A_PIN,OUTPUT);
-	pinMode(MFL_B_PIN,OUTPUT);
-	pinMode(MFL_PWM_PIN,OUTPUT);
+	pinMode(MFL_A_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MFL_B_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MFL_PWM_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
 	 
 	// Motor Back Right Side
-	pinMode(MBR_A_PIN,OUTPUT);
-	pinMode(MBR_B_PIN,OUTPUT);
-	pinMode(MBR_PWM_PIN,OUTPUT);
+	pinMode(MBR_A_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MBR_B_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MBR_PWM_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
 	
 	// Motor Back Left Side
-	pinMode(MBL_A_PIN,OUTPUT);
-	pinMode(MBL_B_PIN,OUTPUT);
-	pinMode(MBL_PWM_PIN,OUTPUT);
+	pinMode(MBL_A_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MBL_B_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(MBL_PWM_PIN,OUTPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
 
-	pinMode(IRL_PIN_s0, INPUT);
-	pinMode(IRL_PIN_s1, INPUT);
-	pinMode(IRL_PIN_s3, INPUT);
-	pinMode(IRL_PIN_s4, INPUT);
-	pinMode(IRL_PIN_s5, INPUT);
-	pinMode(IRL_PIN_s6, INPUT);
-	pinMode(IRL_PIN_s7, INPUT);
+	pinMode(IRL_PIN_s0, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s1, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s3, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s4, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s5, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s6, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
+	pinMode(IRL_PIN_s7, INPUT);		// Sets the pin mode of the I/O pin indicated by the #defined object as output
 	
 }
 
-int IRL_in;
-int diff = 0;
+int IRL_in;					// declaration of the buffer to store the output of all  s0- s7 pins of the KRF
+int diff = 0;					// decalaration of the variablediff; is used for the variation of the turning value.  
 int mPower = map(50, 0, 100, 80, 190);
 
 void loop()
@@ -71,9 +86,10 @@ void loop()
 	
 	for(int i = 0; i <= 7; i++)
 	{
-		IRL_in = (IRL_in << 1) + digitalRead(IRL_PIN_s0 + i);
+		IRL_in = (IRL_in << 1) + digitalRead(IRL_PIN_s7 - i);
 		Serial.println(IRL_in, BIN);
 	}
+	Serial.println(IRL_in, BIN);
 	
 	switch(IRL_in)
 	{
@@ -100,7 +116,7 @@ void loop()
 			analogWrite(MBR_PWM_PIN, mPower);
 			analogWrite(MBL_PWM_PIN, mPower);
 			break;
-		case 0b10011111:
+		case 0b00011111:
 		case 0b10001111:
 			
 			Serial.println("LL000");
@@ -125,32 +141,8 @@ void loop()
 			analogWrite(MBR_PWM_PIN, mPower - diff);
 			analogWrite(MBL_PWM_PIN, mPower);
 			break;
-		case 0b11001111:
-
-                        Serial.println("0L000");
-		
-			diff = 50;
 			
-			digitalWrite(MFR_A_PIN, HIGH);
-			digitalWrite(MFR_B_PIN, LOW);
-			
-			digitalWrite(MBR_A_PIN, HIGH);
-			digitalWrite(MBR_B_PIN, LOW);
-						
-			digitalWrite(MFL_A_PIN, HIGH);
-			digitalWrite(MFL_B_PIN, LOW);
-				
-			digitalWrite(MBL_A_PIN, HIGH);
-			digitalWrite(MBL_B_PIN, LOW);
-		
-			analogWrite(MFR_PWM_PIN, mPower - diff);
-			analogWrite(MFL_PWM_PIN, mPower);
-			
-			analogWrite(MBR_PWM_PIN, mPower - diff);
-			analogWrite(MBL_PWM_PIN, mPower);
-			break;
 		case 0b11000111:
-
                         Serial.println("0LL00");
 		
 			diff = 20;
