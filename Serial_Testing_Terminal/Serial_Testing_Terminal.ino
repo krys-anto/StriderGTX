368c243bd302db72c9abebc22c591ade42c95b08
kristoff howell <kris.anto.howell@gmail.com>
2015-04-06 04:49:27 -0500

    updated to the individual motor speed control

--- Serial_Testing_Terminal/Serial_Testing_Terminal.ino
+++ Serial_Testing_Terminal/Serial_Testing_Terminal.ino
@@ -2,21 +2,21 @@
 
 Servo myservo;
 
-#define MFR_A_PIN	34   	// FRONT RIGHT MOTOR
-#define MFR_B_PIN	36 
-#define MFR_PWM_PIN	7 	// PULSE WIDTH MODULATION PIN
+#define MFR_A_PIN	11   	// FRONT RIGHT MOTOR
+#define MFR_B_PIN	12
+#define MFR_PWM_PIN	13	// PULSE WIDTH MODULATION PIN
 
-#define MFL_A_PIN	48	//FRONT LEFT MOTOR
-#define MFL_B_PIN 	46
-#define MFL_PWM_PIN	5	 // PULSE WIDTH MODULATION PIN
+#define MFL_A_PIN	5	//FRONT LEFT MOTOR
+#define MFL_B_PIN 	6
+#define MFL_PWM_PIN	7	 // PULSE WIDTH MODULATION PIN
 
-#define MBR_A_PIN	38	//BACK RIGHT MOTOR
-#define MBR_B_PIN	40
+#define MBR_A_PIN	9	//BACK RIGHT MOTOR
+#define MBR_B_PIN	10
 #define MBR_PWM_PIN	8	// PULSE WIDTH MODULATION PIN
 
-#define MBL_A_PIN	44	//BACK LEFT MOTOR
-#define MBL_B_PIN	42
-#define MBL_PWM_PIN	6 	// PULSE WIDTH MODULATION PIN 
+#define MBL_A_PIN	3	//BACK LEFT MOTOR
+#define MBL_B_PIN	4
+#define MBL_PWM_PIN	2 	// PULSE WIDTH MODULATION PIN 
 
 void setup()
 {
@@ -58,11 +58,19 @@ void setup()
 
 
 int mSpeed = 0;
+
+int mSpeedFR = 0;
+int mSpeedBR = 0;
+
+int mSpeedFL = 0;
+int mSpeedBL = 0;
+
 int PIN = -1;
 int ANG = -1;
 
-char buffer[] = {' ',' ',' ',' ',' ',' ',' '}; // Receive up to 7 bytes
-char serialInput = '0';
+char buffer[] = {' ',' ',' ',' ',' ',' ',' '}; // Receive up to 7 bytes (Used to store the input from communication)
+char pwm[] = {' ', ' ', ' '};
+char serialInput;
 
 void loop()
 {
@@ -71,223 +79,188 @@ void loop()
 	
 	while (!Serial.available());
 	
-	if (Serial.available() > 0) 
-	{
+
 		switch(Serial.read())
 		{
 			case 'N':
 				Serial.println("Navigation Test");
 				
 				mSpeed = 0;
-				if (Serial.available() > 0) 
-				{	
-					while(serialInput != '<')
+				while(serialInput != '<')
+				{
+					while (!Serial.available());
+					serialInput = Serial.read();
+					switch(serialInput)
 					{
-						serialInput = Serial.read();
-						switch(serialInput)
-						{
-							case 'F': // Forward Test
-				
-								Serial.println("Motor Forward Test Low, High");
-									
-								// Setting  direction 
+						case 'F': // Forward Test
+			
+							Serial.println("Motor Forward Test Low, High");
 								
-								digitalWrite(MFR_A_PIN, LOW);
-								digitalWrite(MFR_B_PIN, HIGH);
-								
-								digitalWrite(MFL_A_PIN, HIGH);
-								digitalWrite(MFL_B_PIN, LOW);
-								
-								digitalWrite(MBR_A_PIN, LOW);
-								digitalWrite(MBR_B_PIN, HIGH);
-								
-								digitalWrite(MBL_A_PIN, HIGH);
-								digitalWrite(MBL_B_PIN, LOW);
-								
-									// Sending Instruction to start movement
-								
-								break;
+							// Setting  direction 
 							
-							case 'B': // Motor BACKWARD
+							digitalWrite(MFR_A_PIN, LOW);
+							digitalWrite(MFR_B_PIN, HIGH);
 							
-								Serial.println("Motor Backward Test High, Low");
-								
-								// Setting  direction
-									
-								digitalWrite(MFR_A_PIN, HIGH);
-								digitalWrite(MFR_B_PIN, LOW);
-								
-								digitalWrite(MFL_A_PIN, LOW);
-								digitalWrite(MFL_B_PIN, HIGH);
-								
-								digitalWrite(MBR_A_PIN, HIGH);
-								digitalWrite(MBR_B_PIN, LOW);
-								
-								digitalWrite(MBL_A_PIN, LOW);
-								digitalWrite(MBL_B_PIN, HIGH);
-								
+							digitalWrite(MFL_A_PIN, LOW);
+							digitalWrite(MFL_B_PIN, HIGH);
+							
+							digitalWrite(MBR_A_PIN, HIGH);
+							digitalWrite(MBR_B_PIN, LOW);
+							
+							digitalWrite(MBL_A_PIN, HIGH);
+							digitalWrite(MBL_B_PIN, LOW);
+							
 								// Sending Instruction to start movement
 							
-								break;
-								
-							case 'R': // Motor TURN RIGHT
-							
-								Serial.println("Motor Right Test High, Low");
-								
-								// Setting  direction
-									
-								digitalWrite(MFR_A_PIN, HIGH);
-								digitalWrite(MFR_B_PIN, LOW);
-								
-								digitalWrite(MFL_A_PIN, HIGH);
-								digitalWrite(MFL_B_PIN, LOW);
-								
-								digitalWrite(MBR_A_PIN, HIGH);
-								digitalWrite(MBR_B_PIN, LOW);
-								
-								digitalWrite(MBL_A_PIN, HIGH);
-								digitalWrite(MBL_B_PIN, LOW);
-								
-								// Sending Instruction to start movement
-							
-								break;
-								
-							case 'L': // Motor TURN LEFT
-							
-								Serial.println("Motor Forward Test High, Low");
-								
-								// Setting  direction
-									
-								digitalWrite(MFR_A_PIN, LOW);
-								digitalWrite(MFR_B_PIN, HIGH);
-								
-								digitalWrite(MFL_A_PIN, LOW);
-								digitalWrite(MFL_B_PIN, HIGH);
-								
-								digitalWrite(MBR_A_PIN, LOW);
-								digitalWrite(MBR_B_PIN, HIGH);
-								
-								digitalWrite(MBL_A_PIN, LOW);
-								digitalWrite(MBL_B_PIN, HIGH);
-								
-								// Sending Instruction to start movement
-							
-								break;
-							
-							case '0':
-								mSpeed = 0;
-								break;
-								
-							case '1':
-								mSpeed = 25;
-								break;
-								
-							case '2':
-								mSpeed = 50;
-								break;
-								
-							case '3':
-								mSpeed = 75;
-								break;
-								
-							case '4':
-								mSpeed = 100;
-								break;
-								
-							case '5':
-								mSpeed = 125;
-								break;
-								
-							case '6':
-								mSpeed = 150;
-								break;
-								
-							case '7':
-								mSpeed = 175;
-								break;
-								
-							case '8':
-								mSpeed = 200;
-								break;
-								
-							case '9':
-								mSpeed = 225;
-								break;
-								
-							case 'I':
-								mSpeed += 2;
-								break;
-						}//end switch
+							break;
 						
-						Serial.print("Speed = ");
-						Serial.println(mSpeed);
+						case 'B': // Motor BACKWARD
 						
-						analogWrite(MFR_PWM_PIN, mSpeed);
-						analogWrite(MFL_PWM_PIN, mSpeed);
+							Serial.println("Motor Backward Test High, Low");
+							
+							// Setting  direction
+								
+							digitalWrite(MFR_A_PIN, HIGH);
+							digitalWrite(MFR_B_PIN, LOW);
+							
+							digitalWrite(MFL_A_PIN, LOW);
+							digitalWrite(MFL_B_PIN, HIGH);
+							
+							digitalWrite(MBR_A_PIN, HIGH);
+							digitalWrite(MBR_B_PIN, LOW);
+							
+							digitalWrite(MBL_A_PIN, LOW);
+							digitalWrite(MBL_B_PIN, HIGH);
+							
+							// Sending Instruction to start movement
 						
-						analogWrite(MBR_PWM_PIN, mSpeed);
-						analogWrite(MBL_PWM_PIN, mSpeed);
-					}//end while
-				}//end If
-				break;
-			case 'A':
-				Serial.println("Arm Test");
-                                        //Serial.println("1");	
-					while(serialInput != '<')
-					{
-                                                //Serial.println("2");
-                                                while (!Serial.available());
-                                                
-						serialInput = Serial.read();
-                                                Serial.println(serialInput);
-                                                
-						switch(serialInput)
-						{
-							case 'P': // Sets the PIN to be ATTached
-								PIN = -1;
-								for(int i = 0; i < 6; i++)
-								{
-									buffer[i] = ' ';
-								}
+							break;
+							
+						case 'R': // Motor TURN RIGHT
+						
+							Serial.println("Motor Right Test High, Low");
+							
+							// Setting  direction
 								
-								while (!Serial.available()); // Wait for character
-								while(!(PIN > 0))
-								{
-									Serial.print("Enter PIN -> ");
-									while (!Serial.available()); // Wait for character
-									Serial.readBytesUntil('n', buffer, 7);
-									PIN = atoi(buffer);
-									
-									Serial.println(PIN);
-								}//end While
-
-                                                                myservo.attach(PIN);
-                                                                myservo.write(180);
-                                                                
-								break;
-							case 'R':
-								ANG = -1;
-								for(int i = 0; i < 6; i++)
-								{
-									buffer[i] = ' ';
-								}
+							digitalWrite(MFR_A_PIN, HIGH);
+							digitalWrite(MFR_B_PIN, LOW);
+							
+							digitalWrite(MFL_A_PIN, HIGH);
+							digitalWrite(MFL_B_PIN, LOW);
+							
+							digitalWrite(MBR_A_PIN, HIGH);
+							digitalWrite(MBR_B_PIN, LOW);
+							
+							digitalWrite(MBL_A_PIN, HIGH);
+							digitalWrite(MBL_B_PIN, LOW);
+							
+							// Sending Instruction to start movement
+						
+							break;
+							
+						case 'L': // Motor TURN LEFT
+						
+							Serial.println("Motor Forward Test High, Low");
+							
+							// Setting  direction
 								
-								while (!Serial.available()); // Wait for character
-								while(!(ANG > 0))
-								{
-                                                                        Serial.print("Enter Angle -> ");
-									while (!Serial.available()); // Wait for character
-									Serial.readBytesUntil('n', buffer, 7);
-									ANG = atoi(buffer);
-									
-                                                                        Serial.println(ANG);
-								}//end While
-								
-								myservo.write(ANG);
-								break;
-						}//end switch
-					}//end while
-				break;
+							digitalWrite(MFR_A_PIN, LOW);
+							digitalWrite(MFR_B_PIN, HIGH);
+							
+							digitalWrite(MFL_A_PIN, LOW);
+							digitalWrite(MFL_B_PIN, HIGH);
+							
+							digitalWrite(MBR_A_PIN, LOW);
+							digitalWrite(MBR_B_PIN, HIGH);
+							
+							digitalWrite(MBL_A_PIN, LOW);
+							digitalWrite(MBL_B_PIN, HIGH);
+							
+							// Sending Instruction to start movement
+						
+							break;
+						
+						case '0':
+							mSpeedFL = 0;
+							mSpeedFR = 0;
+							
+							mSpeedBL = 0;
+							mSpeedBR = 0;
+							break;
+							
+						case 'i':
+						case 'I':
+							Serial.print("INput FL => ");
+							while(!Serial.available()); // waits untill serial communcation line is not avalible meaning that data was sent from the opposite host 
+							Serial.readBytesUntil('n', pwm, 3);
+							
+							mSpeedFL = atoi(pwm);
+							Serial.println(mSpeedFL);
+							
+							for(int i = 0; i < 3; i++)
+							{
+								pwm[i] = ' ';
+							}
+							
+							Serial.print("INput FR => ");
+							while(!Serial.available()); // waits untill serial communcation line is not avalible meaning that data was sent from the opposite host 
+							Serial.readBytesUntil('n', pwm, 3);
+							
+							mSpeedFR = atoi(pwm);
+							Serial.println(mSpeedFR);
+							
+							for(int i = 0; i < 3; i++)
+							{
+								pwm[i] = ' ';
+							}
+							
+							Serial.print("INput BL => ");
+							while(!Serial.available()); // waits untill serial communcation line is not avalible meaning that data was sent from the opposite host 
+							Serial.readBytesUntil('n', pwm, 3);
+							
+							mSpeedBL = atoi(pwm);
+							Serial.println(mSpeedBL);
+							
+							for(int i = 0; i < 3; i++)
+							{
+								pwm[i] = ' ';
+							}
+							
+							Serial.print("INput BR => ");
+							while(!Serial.available()); // waits untill serial communcation line is not avalible meaning that data was sent from the opposite host 
+							Serial.readBytesUntil('n', pwm, 3);
+							
+							mSpeedBR = atoi(pwm);
+							Serial.println(mSpeedBR);
+							
+							for(int i = 0; i < 3; i++)
+							{
+								pwm[i] = ' ';
+							}
+							
+							break;
+					}//end switch
+					
+					analogWrite(MFR_PWM_PIN, mSpeedFR);
+					analogWrite(MFL_PWM_PIN, mSpeedFL);
+					
+					analogWrite(MBR_PWM_PIN, mSpeedBR);
+					analogWrite(MBL_PWM_PIN, mSpeedBL);
+					
+					Serial.print("SpeedFL = ");
+					Serial.print(mSpeedFL);
+					Serial.print("	");
+					Serial.print("SpeedFR = ");
+					Serial.println(mSpeedFR);
+					
+					Serial.print("SpeedBL = ");
+					Serial.print(mSpeedBL);
+					Serial.print("	");
+					Serial.print("SpeedBR = ");
+					Serial.println(mSpeedBR);
+					
+				}//end while
+			break;
 		} // end Switch
-	} // end If
+
 } // end void loop()
